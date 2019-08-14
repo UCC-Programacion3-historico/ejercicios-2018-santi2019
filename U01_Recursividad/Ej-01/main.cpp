bb2999948e3977041a36d07e4b18a92d32ef5948
santi2019 <santiagovietto5@hotmail.com>
2019-08-14 10:38:42 -0300

    ejercicio 1 jeje

--- U01_Recursividad/Ej-01/main.cpp
+++ U01_Recursividad/Ej-01/main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 #include "potencia.h"
 
+int potencia(int base,int exp){
+    if(exp==0)
+        return 1;
+    else
+    return base * potencia(base, exp - 1);
+}
+
 int main() {
+    int base,exp;
+
+    cout<<"ingrese base: ";
+    cin>>base;
+    cout<<endl;
+    cout<<"ingrese base: ";
+    cin>>base;
+
+
     std::cout << "Ejercicio 01/01\n" << std::endl;
     return 0;
 }
--- U01_Recursividad/Ej-01/potencia.h
+++ U01_Recursividad/Ej-01/potencia.h
@@ -2,7 +2,7 @@
 #define POTENCIA_H
 
 
-unsigned int potencia(unsigned int base, unsigned int exp);
+unsigned int potencia(unsigned int base, unsigned int exp); //estos permiten obtener bases y potencias positivas
 
 
 #endif //INC_01_RECURSIVIDAD_POTENCIA_H
